6eacbd92fd35e37982ba191ab4cbcd19e30e14ec
Aras <vahitaras@gmail.com>
2023-05-24 14:45:31 -0400

    setUserPass and getUserPass methods added

--- models/dataHandling.js
+++ models/dataHandling.js
@@ -34,10 +34,23 @@ const bulkCreateUserAndPass = async () => {
 
     for (let i = 0; i < createdUserPasses.length; i++) {
       // Model-Association-for-hasOne-method
-      // set+UserPass is coming from the model, sequelize automatically
+      // set+UserPass is coming from the model name, sequelize automatically
       // creates this method so that these 2 models can be associated.
       await createdUsers[i].setUserPass(createdUserPasses[i]);
     }
+  } catch (err) {
+    console.error(err);
+  }
+
+  try {
+    const foundUser = await User.findOne({ where: { name: "aras" } });
+    // Model-Association-for-hasOne-method
+    // get+UserPass is coming from the model, sequelize automatically 
+    // creates this method so that these 2 models can be associated.
+    const foundUserPass = await foundUser.getUserPass();
+
+    // This should show the foundUserPass
+    console.log(foundUserPass);
   } catch (err) {
     console.error(err);
   }
