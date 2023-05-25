557ecb745f5bce471e39e74c7b3acc2d50707e39
Aras <vahitaras@gmail.com>
2023-05-25 15:24:06 -0400

    extra notes added for hasOne and belongsTo associations

--- models/dbModelAssociations.js
+++ models/dbModelAssociations.js
@@ -7,12 +7,16 @@ const UserPass = require("./tables/userPassTable");
 // "foreignKey" property changes default foreign key from "userId" to "userIDs"
 // This enables methods like set+UserPass, get+UserPass, create+UserPass
 // in hasOne, second argument is optional.
+// Alternatively
+// User.hasOne(UserPass, { foreignKey: { name: 'userIDs', allowNull: false, type: DataTypes.INTEGER } });
 User.hasOne(UserPass, { foreignKey: "userIDs" });
 // Having both associations enables you to perform operations like 
 // retrieving a user's associated UserPass record or retrieving a 
 // User based on a UserPass record, depending on your application's 
 // requirements and data access patterns.
 // in belongsTo, second argument is optional.
+// Alternatively
+// UserPass.belongsTo(User, { foreignKey: { name: 'userIDs', allowNull: false, type: DataTypes.INTEGER } });
 UserPass.belongsTo(User, { foreignKey: "userIDs" });
 
 module.exports = {
