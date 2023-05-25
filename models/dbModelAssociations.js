e90f3392b2c16d030d288cc3239739ade2a61133
Aras <vahitaras@gmail.com>
2023-05-25 15:18:17 -0400

    extra notes added to model associations

--- models/dbModelAssociations.js
+++ models/dbModelAssociations.js
@@ -6,11 +6,13 @@ const UserPass = require("./tables/userPassTable");
 // is set in this file so the program knows where to put the foreign key
 // "foreignKey" property changes default foreign key from "userId" to "userIDs"
 // This enables methods like set+UserPass, get+UserPass, create+UserPass
+// in hasOne, second argument is optional.
 User.hasOne(UserPass, { foreignKey: "userIDs" });
 // Having both associations enables you to perform operations like 
 // retrieving a user's associated UserPass record or retrieving a 
 // User based on a UserPass record, depending on your application's 
 // requirements and data access patterns.
+// in belongsTo, second argument is optional.
 UserPass.belongsTo(User, { foreignKey: "userIDs" });
 
 module.exports = {
